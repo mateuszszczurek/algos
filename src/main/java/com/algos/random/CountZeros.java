7664c950ab100549093010243f8e6ae510a67bd0
Mateusz <mateusz.szczurek86@gmail.com>
2019-01-15 19:56:07 +0100

    Counting zeros in sorted matrix (binary search)

--- src/main/java/com/algos/random/CountZeros.java
+++ src/main/java/com/algos/random/CountZeros.java
@@ -4,16 +4,16 @@ public class CountZeros {
 
     public static void main(String[] args) {
 
-        int[][] array1 = {{0, 0, 1}, {0, 0, 1}, {0, 1, 1}};
-        int numberOfZeros = new CountZeros().countZero(array1);
+        int[][] input = {{0, 0, 1}, {0, 0, 1}, {0, 1, 1}};
+        CountZeros countZeros = new CountZeros();
 
-        System.out.println("Number of zeros: " + numberOfZeros);
+        System.out.println("Number of zeros: " + countZeros.countZero(input));
+        System.out.println("Number of zeros (using binary search): " + countZeros.countZerosBinarySearch(input));
     }
 
     public int countZero(int[][] array) {
 
-        int row = 0;
-        int column = array[row].length - 1;
+        int row = 0, column = array[row].length - 1;
 
         int total = 0;
 
@@ -27,6 +27,49 @@ public class CountZeros {
         }
 
         return total;
+    }
+
+    public int countZerosBinarySearch(int[][] array) {
+
+        int row = 0, amountOfZeros = 0;
+
+        int endIndex = array[row].length - 1;
+
+        while (row < array.length && endIndex != -1) {
+
+            endIndex = findStartingZero(0, endIndex, array[row]);
+
+            if (endIndex != -1) {
+                amountOfZeros += (endIndex + 1);
+            }
+            row++;
+        }
+
+        return amountOfZeros;
+    }
+
+    private int findStartingZero(int startIndex, int endIndex, int[] array) {
+        int searchRange = (endIndex - startIndex) + 1;
+
+        if (searchRange < 2) {
+            return -1;
+        } else if (searchRange == 2) {
+            if (array[endIndex] == 0) {
+                return endIndex;
+            } else if (array[startIndex] == 0) {
+                return startIndex;
+            } else {
+                return -1;
+            }
+        }
+
+        int middle = (endIndex - startIndex) / 2;
+
+        if (array[middle] == 0) {
+            return findStartingZero(middle, endIndex, array);
+        } else {
+            return findStartingZero(startIndex, middle, array);
+        }
     }
 
 }
