68e6b05ab65bf4e2d898e7ae0b7fa3b8291dbe59
Mateusz <mateusz.szczurek86@gmail.com>
2019-12-17 23:55:54 +0100

    Array rotation task

--- src/main/java/com/algos/arrays/ArrayRotate.java
+++ src/main/java/com/algos/arrays/ArrayRotate.java
@@ -1,10 +1,50 @@
 package com.algos.arrays;
 
+import java.util.Arrays;
+
 import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.newArrayListWithCapacity;
+import static java.lang.System.*;
 import static java.util.stream.Collectors.joining;
 
-//https://www.geeksforgeeks.org/array-rotation/ - do according to spec
+//https://practice.geeksforgeeks.org/problems/rotate-and-delete/0 - do according to spec
 public class ArrayRotate {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+//        int[] array = new int[]{1, 2, 3, 4, 5, 6};
+        int[] array = new int[]{1, 2, 3, 4};
+
+        int k = 0;
+        while (array.length > 1) {
+            int[] newArray = new int[array.length - 1];
+
+            rightRotate(array);
+
+            if (k == array.length - 1) {
+                arraycopy(array, 1, newArray, 0, array.length - 1);
+            } else if (k == 0) {
+                arraycopy(array, 0, newArray, 0, array.length - 1);
+            } else {
+                int removalIndex = array.length - 1 - k;
+                arraycopy(array, 0, newArray, 0, removalIndex);
+                arraycopy(array, removalIndex + 1, newArray, removalIndex, array.length - (removalIndex + 1));
+            }
+
+            if (k < newArray.length - 1) {
+                k++;
+            } else {
+                k = newArray.length - 1;
+            }
+            array = newArray;
+        }
+
+        out.println(Arrays.toString(array));
+    }
+
+    private static void rightRotate(int[] array) {
+        int temp = array[array.length - 1];
+        arraycopy(array, 0, array, 1, array.length - 1);
+        array[0] = temp;
+    }
+
 }
